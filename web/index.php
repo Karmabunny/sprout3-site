3ba384c567ab0dcbfcf963baf90811b3f3ef632d
gwillz <me@gwilyn.com>
2021-12-06 08:17:55 +1030

    Rewrite simplified index. Move index and files into /web. Add storage. Update rewrites.

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright (C) 2017 Karmabunny Pty Ltd.
+ *
+ * This file is a part of SproutCMS.
+ *
+ * SproutCMS is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * For more information, visit <http://getsproutcms.com>.
+ */
+
+// A big bunch of core constants.
+define('BASE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('VENDOR_PATH', BASE_PATH . 'vendor' . DIRECTORY_SEPARATOR);
+define('STORAGE_PATH', BASE_PATH . 'storage' . DIRECTORY_SEPARATOR);
+define('DOCROOT', BASE_PATH . 'src' . DIRECTORY_SEPARATOR);
+define('KOHANA', basename(__FILE__));
+
+// This file contains a class with a methods for determining the details of
+// the very initial environment, prior to the rest of the system coming up
+require DOCROOT . 'config/_bootstrap_config.php';
+
+// The composer autoloader.
+require VENDOR_PATH . 'autoload.php';
+
+// Load up.
+require VENDOR_PATH . 'karmabunny/sprout/src/bootstrap.php';
