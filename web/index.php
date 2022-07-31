dd697c6117527b25b15b8bd9327c62ebdb675d62
Gwilyn <gwilyn@karmabunny.com.au>
2022-07-31 17:19:24 +0930

    Update app entry and rewrites.

--- web/index.php
+++ web/index.php
@@ -11,26 +11,19 @@
  * For more information, visit <http://getsproutcms.com>.
  */
 
+// Catching max_input_vars and other pre-execution errors.
+// see https://stackoverflow.com/a/21601349/1688568
+$e0 = error_get_last();
+
 // A big bunch of core constants.
 define('BASE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('VENDOR_PATH', BASE_PATH . 'vendor' . DIRECTORY_SEPARATOR);
 define('STORAGE_PATH', BASE_PATH . 'storage' . DIRECTORY_SEPARATOR);
 define('DOCROOT', BASE_PATH . 'src' . DIRECTORY_SEPARATOR);
+define('WEBROOT', BASE_PATH . 'web' . DIRECTORY_SEPARATOR);
 define('KOHANA', basename(__FILE__));
 
 ini_set('display_errors', '1');
 
-// This file contains a class with a methods for determining the details of
-// the very initial environment, prior to the rest of the system coming up
-require DOCROOT . 'config/_bootstrap_config.php';
-
-// Define the website environment status.
-if (file_exists(DOCROOT . 'config/dev_hosts.php')) {
-    require DOCROOT . 'config/dev_hosts.php';
-}
-
-// The composer autoloader.
 require VENDOR_PATH . 'autoload.php';
-
-// Load up.
-require VENDOR_PATH . 'karmabunny/sprout/src/bootstrap.php';
+require VENDOR_PATH . 'sproutcms/cms/src/bootstrap.php';
