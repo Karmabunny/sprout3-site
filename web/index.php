01b2b1122b8211c5cc9fcc1ab1375c3903ff214b
Gwilyn <gwilyn@karmabunny.com.au>
2023-04-03 14:58:16 +0930

    Update entry file inline with upstream.

--- web/index.php
+++ web/index.php
@@ -15,15 +15,19 @@
 // see https://stackoverflow.com/a/21601349/1688568
 $e0 = error_get_last();
 
-// A big bunch of core constants.
 define('BASE_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('VENDOR_PATH', BASE_PATH . 'vendor' . DIRECTORY_SEPARATOR);
-define('STORAGE_PATH', BASE_PATH . 'storage' . DIRECTORY_SEPARATOR);
-define('DOCROOT', BASE_PATH . 'src' . DIRECTORY_SEPARATOR);
-define('WEBROOT', BASE_PATH . 'web' . DIRECTORY_SEPARATOR);
-define('KOHANA', basename(__FILE__));
+
+/**
+ * Optional const paths used by the bootstrap.
+ *
+ * - STORAGE_PATH - ./storage
+ * - DOCROOT - ./src
+ * - WEBROOT - ./web
+ * - KOHANA - index.php
+ */
 
 ini_set('display_errors', '1');
 
 require VENDOR_PATH . 'autoload.php';
-require VENDOR_PATH . 'sproutcms/cms/src/bootstrap.php';
+return require VENDOR_PATH . 'sproutcms/cms/src/bootstrap.php';
